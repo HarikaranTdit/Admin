f72f788848d2d7ea41d5221af7f0cbdb7585de04
harikaranS <harikaransrinivasan17@gmail.com>
2026-08-24 18:58:19 +0530

    Simple code

--- src/main/java/org/tdit/admin/TestCopilat.java
+++ src/main/java/org/tdit/admin/TestCopilat.java
@@ -1,45 +1,37 @@
 package org.tdit.admin;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestCopilat {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
-        try {
-            driver.get("https://qa-bo-admin.smartsortsupport.com/login");
-            driver.manage().window().maximize();
+        driver.get("https://qa-bo-admin.smartsortsupport.com/login");
+        driver.manage().window().maximize();
 
-            System.out.println("Admin is launched successfully");
+        System.out.println("Admin is launched successfully");
+        Thread.sleep(3000);
 
-            WebElement email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email1")));
-            email.sendKeys("SSAdmin");
+        WebElement email = driver.findElement(By.id("email1"));
+        email.sendKeys("SSAdmin");
 
-            WebElement password = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                    By.xpath("//input[@type='password']")));
-            password.sendKeys("S$Admin#");
+        WebElement password = driver.findElement(By.xpath("//input[@type='password']"));
+        password.sendKeys("SSAdmin@123");
 
-            WebElement login = wait.until(ExpectedConditions.elementToBeClickable(
-                    By.xpath("//span[contains(normalize-space(), 'Sign In')]")));
-            login.click();
+        WebElement login = driver.findElement(By.xpath("//span[contains(text(),'Sign In') ]"));
+        login.click();
 
-            WebElement overallMap = wait.until(ExpectedConditions.elementToBeClickable(
-                    By.xpath("//span[contains(normalize-space(), 'Overall Map')]")));
-            overallMap.click();
+        Thread.sleep(3000);
 
-            wait.until(ExpectedConditions.urlContains("map"));
-            System.out.println("Overall Map screen opened successfully");
-        } finally {
-            driver.quit();
-        }
+        WebElement overallMap = driver.findElement(By.xpath("//span[contains(text(),'Overall Map') ]"));
+        overallMap.click();
+
+        System.out.println("Overall Map screen opened successfully");
+
+        driver.quit();
     }
 }
